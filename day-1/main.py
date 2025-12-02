3a6f6e7c1b1f1172f5c60f65cef434c6df7e1be7
cloverlucy <itscloverlucy@gmail.com>
2025-12-02 12:59:19 +0000

    simplified day 1 statements

--- day-1/main.py
+++ day-1/main.py
@@ -18,9 +18,9 @@ def secret_entrance_part2(data, start_number):
     for i in data:
         adjustment = int(i[1:])
         if i[0] == "L":
-            if number > 0 and adjustment >= number:
+            if adjustment >= number > 0:
                 count += 1 + ((adjustment - number) // 100)
-            elif number == 0 and adjustment >= 100:
+            elif number == 0:
                 count += adjustment // 100
             number -= adjustment
         elif i[0] == "R":
@@ -34,4 +34,4 @@ with open("data.txt") as file:
     data_file = file.readlines()
 
 secret_entrance_part1(data_file, 50)
-secret_entrance_part2(data_file, 50)
+print(secret_entrance_part2(data_file, 50))
